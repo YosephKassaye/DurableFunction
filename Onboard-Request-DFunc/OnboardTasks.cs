3fcb936d177d8b4e9914c26a840ac49cfcbe3923
Yoseph kassaye <yadugna@gmail.com>
2021-12-27 23:18:13 -0800

    more updates

--- Onboard-Request-DFunc/OnboardTasks.cs
+++ Onboard-Request-DFunc/OnboardTasks.cs
@@ -27,9 +27,9 @@ namespace Onboard_Request_DFunc
                 // Replace "hello" with the name of your Durable Activity Function.
                 var request = JsonConvert.DeserializeObject<RequestObject>(context.InstanceId);
                 if(request.SystemTypeId=="EASI")
-                    outputs.Add(await context.CallActivityAsync<string>("OnboardEASITasks", request.RequestId));
+                    outputs.Add(await context.CallActivityAsync<string>("OnboardEASITasks", request));
                 else
-                    outputs.Add(await context.CallActivityAsync<string>("OnboardFISTasks", request.RequestId));
+                    outputs.Add(await context.CallActivityAsync<string>("OnboardFISTasks", request));
             }
             catch (System.Exception ex )
             {
@@ -43,17 +43,35 @@ namespace Onboard_Request_DFunc
         }
 
         [FunctionName("OnboardEASITasks")]
-        public static string OnboardEASITasks([ActivityTrigger] string name, ILogger log)
+        public static string OnboardEASITasks([ActivityTrigger] RequestObject requestObject, ILogger log)
         {
-            log.LogInformation($"EASI Task Handler {name}.");
-            return $"Hello EASI {name}!";
+
+            log.LogInformation($"EASI Task Handler {requestObject.SystemTypeId}.");
+            return $"Hello EASI {requestObject.RequestId}!";
         }
         [FunctionName("OnboardFISTasks")]
-        public static string OnboardFISTasks([ActivityTrigger] string name, ILogger log)
+        public static string OnboardFISTasks([ActivityTrigger] RequestObject requestObject, ILogger log)
         {
-            log.LogInformation($"FIS Task Handler {name}.");
-            return $"Hello FIS {name}!";
+
+            log.LogInformation($"FIS Task Handler {requestObject.SystemTypeId}.");
+            return $"Hello FIS {requestObject.RequestId}!";
         }
+
+        //[FunctionName("OnboardEASITasks")]
+        //public static async Task OnboardEASITasks([ActivityTrigger] RequestObject requestObject, ILogger log)
+        //{
+        //    var cosmosUrl = "https://yayobecosmosdb.documents.azure.com:443/";
+        //    var cosmoskey = "LVFheqUyNbqF6FweueKMntxVLZPHJRcDOc8m65kJIxQicYh9oHr4Vee0RfdyV2hQFowFxODdhcPEODTFcpxRcg==";
+        //    var databaseName = "TaskHandler";
+        //    CosmosClient client = new CosmosClient(cosmosUrl, cosmoskey);
+        //    Database database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+        //    Container container = await database.CreateContainerIfNotExistsAsync("EasiTaskHandler", "/RequestId", 400);
+
+        //    var taskhandler = await container.CreateItemAsync(requestObject);
+
+        //    log.LogInformation($"EASI Task Handler {requestObject.SystemTypeId}.");
+        //    //return $"Hello EASI {requestObject.RequestId}!";
+        //}
 
         [FunctionName("OnboardTasks_HttpStart")]
         public static  async Task  HttpStart(
